a9ac55f2288508fe9c72d4a5cab2295012a4f9cc
John Morris <jmorris@dacis.com>
2024-10-17 21:13:15 -0500

    acme-challenge

--- src/connection.js
+++ src/connection.js
@@ -1,15 +1,11 @@
-require('dotenv').config({ path: `${__dirname}/../.env` });
+require("dotenv").config({ path: `${__dirname}/../.env` });
 
-let dbHost = 'data.dacis.com';
-if (process.env.NODE_ENV === 'production') {
-  dbHost = '127.0.0.1';
-}
 const mysqlServer = {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: 'waters_refuge_ball',
+  database: "waters_refuge_ball",
 };
 
 module.exports = { mysqlServer };
--- src/index.js
+++ src/index.js
@@ -1,92 +1,47 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const fs = require('fs');
-const http = require('http');
-const https = require('https');
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const cors = require('cors');
-const vhost = require('vhost');
-const passport = require('passport');
-const { ExtractJwt, Strategy } = require('passport-jwt');
-const mysql = require('mysql2/promise');
-const { mysqlServer } = require('./connection');
-
-// passport.use(
-//   new Strategy(
-//     {
-//       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-//       secretOrKey: process.env.HMAC_JWT,
-//     },
-//     async (payload, done) => {
-//       if (payload.id) {
-//         const sql = `SELECT *
-//         FROM users
-//         WHERE id=?;`;
-//         const conn = await mysql.createConnection(mysqlServer);
-//         const results = await conn.query(sql, [payload.id]);
-//         conn.end();
-
-//         return done(null, results[0]);
-//       }
-
-//       return done(false);
-//     }
-//   )
-// );
+const fs = require("fs");
+const http = require("http");
+const https = require("https");
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const cors = require("cors");
+const vhost = require("vhost");
+const passport = require("passport");
+const { ExtractJwt, Strategy } = require("passport-jwt");
+const mysql = require("mysql2/promise");
+const { mysqlServer } = require("./connection");
 
 // redirect http->https
 const redirectController = (req, res, next) => {
   try {
-    if (!req.secure && process.env.NODE_ENV === 'production') {
+    if (!req.secure && process.env.NODE_ENV === "production") {
       res.redirect(302, `https://` + req.hostname + req.url);
     } else {
       next();
     }
   } catch (error) {
-    console.log('An error has occurred: ', error);
+    console.log("An error has occurred: ", error);
     next(error);
   }
 };
 
 // www server app
 var www = express();
-const api = require('./refuge-front/routes');
+const api = require("./refuge-front/routes");
 
-www.use(redirectController, express.static(path.join(__dirname, 'refuge-front/dist')));
+www.use(
+  redirectController,
+  express.static(path.join(__dirname, "refuge-front/dist"))
+);
 
-www.use('/api', api);
+www.use("/api", api);
 
 // generic route to direct all traffice other than /api to the vue app
-www.use('*', async (req, res) => {
-  res.sendFile(__dirname + '/refuge-front/dist/index.html');
+www.use("*", async (req, res) => {
+  res.sendFile(__dirname + "/refuge-front/dist/index.html");
 });
-// admin app
-// const admin = express();
-// const adminAuthApi = require('./admin-api/auth');
-// const adminApi = require('./admin-api/routes');
-
-// admin.use(express.static(path.join(__dirname, 'admin-front')));
-// admin.use('/reservations', async (req, res) => {
-//   res.sendFile(__dirname + '/admin-front/index.html');
-// });
-// admin.use('/waitlist', async (req, res) => {
-//   res.sendFile(__dirname + '/admin-front/index.html');
-// });
-// admin.use('/openseats', async (req, res) => {
-//   res.sendFile(__dirname + '/admin-front/index.html');
-// });
-// admin.use('/login', async (req, res) => {
-//   res.sendFile(__dirname + '/admin-front/index.html');
-// });
-
-// admin.use('/admin-api', adminAuthApi);
-// admin.use(
-//   '/admin-api',
-//   passport.authenticate('jwt', { session: false }),
-//   adminApi
-// );
 
 // app
 const app = express();
@@ -94,7 +49,7 @@ app.use(cors());
 app.use(
   bodyParser.urlencoded({
     extended: true,
-    limit: '10mb',
+    limit: "10mb",
     parameterLimit: 10000,
   })
 );
@@ -103,16 +58,16 @@ app.use(bodyParser.json());
 // certbot use
 // in the terminal run: certbot certonly --manual -d refugeball.com
 app.use(
-  '/.well-known/acme-challenge/oec3GTKKNjUlr49mSNZ4TBCVcx18-MxkQSSI5KIayhM',
+  "/.well-known/acme-challenge/LrBPzJykvLDbVh1sHOlW3ZLHJggYbuIc5AFopgM2fcQ",
   (req, res) => {
     res.send(
-      'oec3GTKKNjUlr49mSNZ4TBCVcx18-MxkQSSI5KIayhM.jnueKkvexSOuTtRFitgKYrV5VV6YQfpJbnF07scx_ZI'
+      "LrBPzJykvLDbVh1sHOlW3ZLHJggYbuIc5AFopgM2fcQ.jnueKkvexSOuTtRFitgKYrV5VV6YQfpJbnF07scx_ZI"
     );
   }
 );
 
 // development cant use ssl, so all possible routes are maped to the main server
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === "development") {
   app.use(www);
   // app.use(admin);
 
@@ -125,12 +80,12 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // ssl: https://itnext.io/node-express-letsencrypt-generate-a-free-ssl-certificate-and-run-an-https-server-in-5-minutes-a730fbe528ca
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   // Vhost app
 
-  app.use(vhost('www.refugeball.com', www));
+  app.use(vhost("www.refugeball.com", www));
   //app.use(vhost('admin.refugeball.com', admin));
-  app.use(vhost('refugeball.com', www));
+  app.use(vhost("refugeball.com", www));
 
   // Starting both http & https servers
   const httpServer = http.createServer(app);
@@ -141,19 +96,17 @@ if (process.env.NODE_ENV === 'production') {
 
   // refugeball.com cert
   const privateKey = fs.readFileSync(
-    '/etc/letsencrypt/live/refugeball.com-0001/privkey.pem',
-    'utf8'
+    "/etc/letsencrypt/live/refugeball.com-0001/privkey.pem",
+    "utf8"
   );
   const certificate = fs.readFileSync(
-    '/etc/letsencrypt/live/refugeball.com-0001/cert.pem',
-    'utf8'
+    "/etc/letsencrypt/live/refugeball.com-0001/cert.pem",
+    "utf8"
   );
   const ca = fs.readFileSync(
-    '/etc/letsencrypt/live/refugeball.com-0001/chain.pem',
-    'utf8'
+    "/etc/letsencrypt/live/refugeball.com-0001/chain.pem",
+    "utf8"
   );
-
-  
 
   const credentials = {
     key: privateKey,
@@ -163,16 +116,16 @@ if (process.env.NODE_ENV === 'production') {
 
   //*.refugeball.com cert
   const privateKey2 = fs.readFileSync(
-    '/etc/letsencrypt/live/refugeball.com-0002/privkey.pem',
-    'utf8'
+    "/etc/letsencrypt/live/refugeball.com-0002/privkey.pem",
+    "utf8"
   );
   const certificate2 = fs.readFileSync(
-    '/etc/letsencrypt/live/refugeball.com-0002/fullchain.pem',
-    'utf8'
+    "/etc/letsencrypt/live/refugeball.com-0002/fullchain.pem",
+    "utf8"
   );
   const ca2 = fs.readFileSync(
-    '/etc/letsencrypt/live/refugeball.com-0002/chain.pem',
-    'utf8'
+    "/etc/letsencrypt/live/refugeball.com-0002/chain.pem",
+    "utf8"
   );
 
   const credentials2 = {
@@ -182,10 +135,10 @@ if (process.env.NODE_ENV === 'production') {
   };
 
   const httpsServer = https.createServer(credentials, app);
-  httpsServer.addContext('www.refugeball.com', credentials2);
+  httpsServer.addContext("www.refugeball.com", credentials2);
   // httpsServer.addContext('admin.refugeball.com', credentials2);
 
   httpsServer.listen(443, () => {
-    console.log('HTTPS Server running on port 443');
+    console.log("HTTPS Server running on port 443");
   });
 }
